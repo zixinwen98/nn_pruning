2240808a002203a64f8f33eecf37daa578b4c3f2
zixinw <zixinw@andrew.cmu.edu>
2022-01-06 03:33:35 -0500

    Create roberta_glue_eval.sh

---
+++ roberta_glue_eval.sh
@@ -0,0 +1,22 @@
+output_dir="./pruning_roberta_base_mnli"
+for dense_prune_method in topK magnitude threshold sigmoied_threshold
+do
+python -m torch.distributed.launch --nproc_per_node=8 glue_pruning.py \
+--model_name_or_path roberta-base \
+--task_name mnli \
+--do_train \
+--do_eval \
+--max_seq_length 128 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
+--learning_rate 3e-4 \
+--num_train_epochs 5 \
+--output_dir $output_dir/$model_name_or_path \
+--logging_steps 10 \
+--seed 0 \
+--weight_decay 0.1 \
+--report_to wandb \
+--dense_pruning_method $dense_prune_method \
+--attention_pruning_method disabled \
+--regularization disabled 
+done
