5cc9408e3d8d3a26b071e73666a6d41518cd2662
zixinw <zixinw@andrew.cmu.edu>
2022-01-06 00:48:18 -0500

    Update roberta_glue.sh

--- roberta_glue.sh
+++ roberta_glue.sh
@@ -1,8 +1,4 @@
-export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
-export PYTHONHASHSEED=0
-export output_dir="./pruning_roberta_base_mnli"
-
-
+output_dir="./pruning_roberta_base_mnli"
 for dense_prune_method in uniqueness # topK magnitude threshold sigmoied_threshold
 do
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 glue_pruning.py \
@@ -22,7 +18,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node
 --seed 0 \
 --weight_decay 0.1 \
 --report_to wandb \
---dense_pruning_method dense_prune_method \
+--dense_pruning_method $dense_prune_method \
 --attention_pruning_method disabled \
 --regularization disabled 
 done
