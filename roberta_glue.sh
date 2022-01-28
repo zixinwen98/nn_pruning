6564127cdc3e0e03ca372227e85632d3aa7f1311
zixinw <zixinw@andrew.cmu.edu>
2022-01-28 16:16:30 -0500

    Update roberta_glue.sh

--- roberta_glue.sh
+++ roberta_glue.sh
@@ -29,10 +29,3 @@ python glue_pruning.py \
 --parallel_adapter_size 16 
 done
 done
-#--output_dir $dir/$model_name_or_path \
-#--apply_lora \
-#--lora_r 8 \
-#--lora_alpha 16 \
-#--apply_adapter \
-#--adapter_type houlsby \
-#--adapter_size 16
