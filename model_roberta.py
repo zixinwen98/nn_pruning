46814585078e2ad770a5754f387551e1117e7e18
zixinw <zixinw@andrew.cmu.edu>
2022-01-28 15:56:32 -0500

    Update model_roberta.py

--- model_roberta.py
+++ model_roberta.py
@@ -842,12 +842,8 @@ class RobertaModel(RobertaPreTrainedModel):
             self.encoder.layer[layer].attention.prune_heads(heads)
 
     @add_start_docstrings_to_model_forward(ROBERTA_INPUTS_DOCSTRING.format("(batch_size, sequence_length)"))
-    @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC,
-        checkpoint=_CHECKPOINT_FOR_DOC,
-        output_type=BaseModelOutputWithPoolingAndCrossAttentions,
-        config_class=_CONFIG_FOR_DOC,
-    )
+    
+
     # Copied from transformers.models.bert.modeling_bert.BertModel.forward
     def forward(
         self,
