8bf3a95e8be056bbef271271361c24b09ee621c3
zixinw <zixinw@andrew.cmu.edu>
2022-02-03 12:38:32 -0500

    Update roberta_glue.sh

--- roberta_glue.sh
+++ roberta_glue.sh
@@ -28,12 +28,12 @@ python glue_pruning.py \
 --attention_pruning_method disabled \
 --regularization disabled \
 --prune_leftover 0.1 \
-#--apply_adapter \
-#--adapter_type houlsby \
-#--adapter_size 64
 --apply_parallel_adapter \
 --parallel_adapter_size 64 \
 --parallel_adapter_type pfeiffer \
+#--apply_adapter \
+#--adapter_type houlsby \
+#--adapter_size 64
 #--apply_lora \
 #--lora_r 8 \
 #--lora_alpha 16 \
